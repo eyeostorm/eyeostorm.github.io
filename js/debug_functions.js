cdd5fdf1b4f986b71dffb8d32990c730514cf1a1
eyeostorm <chris.martin.cm000@gmail.com>
2025-01-22 20:17:36 -0600

    added merged javascript

--- js/debug_functions.js
+++
@@ -1,7 +0,0 @@
-function debug_waypointXYFacing(){
-    let str = ""
-    for (let j=0; j < global_waypoints.length; j++){
-        str += global_waypoints[j][1] + " " + global_waypoints[j][2] + " " + global_waypoints[j][3]  + "\n"
-    }
-    console.log(str)
-}
--- js/window_resize.js
+++
@@ -1,6 +0,0 @@
-function resizeHandler() {
-    console.log('window resized', global_path_gen_image.clientWidth, window.innerWidth, window.innerHeight)
-    
-}
-
-window.addEventListener('resize', resizeHandler)
