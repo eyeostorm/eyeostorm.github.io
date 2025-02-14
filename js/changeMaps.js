1321bdb6fb7d652bffdaae48bf31993d8bec0ea2
eyeostorm <chris.martin.cm000@gmail.com>
2025-02-14 16:29:44 -0600

    update images to modern vexU

--- js/changeMaps.js
+++ js/changeMaps.js
@@ -1,9 +1,9 @@
 function changeMapToAutonomous(){
-    document.getElementById("pathgenMap").src = "images/autonomous_map.png";
+    document.getElementById("pathgenMap").src = "images/VexU_Autonomous_png.png";
 }
 
 function changeMapToSkills(){
-    document.getElementById("pathgenMap").src = "images/skills_map.png";
+    document.getElementById("pathgenMap").src = "images/VexU_Skills_png.png";
 }
 
 document.getElementById("autonMapChangeLink").addEventListener("click", changeMapToAutonomous);
